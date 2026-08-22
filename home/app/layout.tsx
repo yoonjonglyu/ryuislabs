805f046b3f1c0804efa714d182e695df0ef131ff
Isa(yunjongryu) <yunjonglyu@gmail.com>
2026-08-22 16:43:11 +0900

    feat: HOME PAGE - 랜딩 컨텐츠 초기 구성

--- home/app/layout.tsx
+++ home/app/layout.tsx
@@ -24,9 +24,19 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="ko"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.css"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
